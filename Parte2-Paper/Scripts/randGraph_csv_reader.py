44fe29b8cbe4dd4f212473f345fe306238c09344
Guillermo-Ribero <guillermo.ribero@urosario.edu.co>
2021-11-07 13:44:33 -0500

    Intento para que sea conexo

--- Parte2-Paper/Scripts/randGraph_csv_reader.py
+++ Parte2-Paper/Scripts/randGraph_csv_reader.py
@@ -6,6 +6,8 @@ from random import randint
 def gen_graph(n: int) -> list:
     l = [] #Lista de listas que representan el grafo en forma de la matriz dispersa, nodoA||nodoB||peso
     lp = []#||Lista para evitar arístas múltiples
+    v = []
+    a = True
     for i in range(int(n+n/2)):
         while True:#pensar como optimizar esto pues lo hace hasta que encuentra uno que pueda agregar
             n1 = randint(15, 50)
@@ -13,10 +15,16 @@ def gen_graph(n: int) -> list:
             tp = [n1, n2]
             tp.sort() #como ya metimos n1.n2 no queremos que este n2.n1 pues sería una arísta múltiple
             t = [n1, n2, randint(1, 15)]
-            #Se me ocurre
-            if n1 != n2 and tp not in lp: #Los numeros deben ser diferentes para evitar bucles
+            #Se me ocurre que si uno de los dos a agregar ya estaentonces agreguelos
+            if a == True and n1 != n2 and tp not in lp:
                 l.append(t)
                 lp.append(tp)
+                a == False
+                break
+            if n1 != n2 and tp not in lp and n1 or n2 in v: #Los numeros deben ser diferentes para evitar bucles
+                l.append(t)
+                lp.append(tp)
+                v.append(n1,n2)
                 break
     return l
 
@@ -27,7 +35,7 @@ def graph2csv(t) -> None: #Saca el grafo creado en un csv
     df.to_csv('graph.csv', index=False)
 
 
-def import_graph(name='graph.csv'):#A partir de un csv crea el grafo
+def import_graph(name='graph.csv'):#A partir de un csv crea el grafo falta poner que el usuario meta el nombre con un input
     df = pd.read_csv(name)
     return df.values.tolist()
 
@@ -35,6 +43,7 @@ def import_graph(name='graph.csv'):#A partir de un csv crea el grafo
 def main():
     if int(input('0 - Cargar Grafo \n1 - Generar Grafo \n Seleccion: ')):
         n = randint(15, 50)
+        print(n)
         t = gen_graph(n)
         graph2csv(t)
     else:
