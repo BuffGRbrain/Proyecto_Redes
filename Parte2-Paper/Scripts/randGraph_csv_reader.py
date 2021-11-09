f57165bb1f6f661d72f50f512cf9e173b054f9e8
Guillermo-Ribero <guillermo.ribero@urosario.edu.co>
2021-11-09 16:41:11 -0500

    Test_conexidad_grafo

--- Parte2-Paper/Scripts/randGraph_csv_reader.py
+++ Parte2-Paper/Scripts/randGraph_csv_reader.py
@@ -1,7 +1,30 @@
-from igraph import *
+#from igraph import *
+import igraph
 import pandas as pd
 from random import randint
 
+def vecinos(graph: df, node: int) ->list:
+    vecindad = []
+    for i in graph:
+        if i[1] == node or i[2] == node:
+            vecindad.append(i)
+    return vecindad
+
+def DFS(graph: df, revisados: list,  node: int) ->list: #Esta funci[on me suena raro  creo que deberia modificarla como return y la funcion como debe ser una recursvisa
+    if node not in visitados:
+        visitados.append(node)
+        for i in vecinos(graph, node):
+            DFS(graph, revisados,i)
+    return visitados
+
+def is_conexo(graph: df,v: list):
+    visitados = []
+    connected_nodes = DFS(graph,visitados,graph[1][1])
+    if connected_nodes == v:
+        return True
+    else:
+        print("fuuuck otro que no es conexo")
+        return False
 
 def gen_graph(n: int) -> list:
     l = [] #Lista de listas que representan el grafo en forma de la matriz dispersa, nodoA||nodoB||peso
@@ -28,7 +51,12 @@ def gen_graph(n: int) -> list:
                 break
     return l
 
-
+def Conex_graph_generator(n: int): #Genera el grafo hasta que sea conexo
+    while True:
+        graph = gen_graph(n)
+        if is_conexo(gen_graph(n)) == True:
+            return graph
+#-------------------------------------------GRAFO Y CSV------------------------------------------------------
 def graph2csv(t) -> None: #Saca el grafo creado en un csv
     df = pd.DataFrame(t)
     df = df.set_axis(['Node A', 'Node B', 'Cost'], axis=1, inplace=False)
@@ -39,6 +67,7 @@ def import_graph(name='graph.csv'):#A partir de un csv crea el grafo falta poner
     df = pd.read_csv(name)
     return df.values.tolist()
 
+#-------------------------------------------GRAFO Y CSV------------------------------------------------------
 
 def main():
     if int(input('0 - Cargar Grafo \n1 - Generar Grafo \n Seleccion: ')):
@@ -47,7 +76,9 @@ def main():
         t = gen_graph(n)
         graph2csv(t)
     else:
-        t = import_graph()
+        a = input('Porfavor ingrese el nombre del archivo de donde se generara el grafo')
+        b = a + '.csv'
+        t = import_graph(b)
     #No entendi com funciona esto del layout
     g = Graph.TupleList(t, weights=True)
     g.vs["label"] = g.vs["name"]
