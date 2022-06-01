57b303be308131406d1bb7967cf760686d7759c3
dleyvacastro <dleyvacastro@gmail.com>
2022-06-01 18:23:03 -0500

    simulatios

--- Parte2-Paper/Scripts/main.py
+++ Parte2-Paper/Scripts/main.py
@@ -4,6 +4,7 @@ from matplotlib.pyplot import plot
 from igraph import *
 from functools import cache
 from DJ import Dijkstra, get_path, list_graph_path,iteraciones
+import pandas as pd
 import time
 import json
 import random
@@ -13,7 +14,6 @@ import time
 
 #This code requieres igraph 0.9.8
 
-start_time = time.time() #Starts the stopwatch to see how much time it took to execute and get the desired result
 
 #Input: G a graph from igraph
 #Output: G a graph from igraph updated, old_graph_weights and new_graph_weights to see the changes CHECK
@@ -35,8 +35,10 @@ def loop_update(G:Graph, n:Double, changes:int)->None:
     iteraciones +=1
     a = random.sample(G.vs['name'], 1) #Picks a random node in the graph
     old_L, old_S = Dijkstra(G,a[0]) #First dijkstra to know the state of the graph
-    print_route_tables(G,old_L,a[0])
+    df = print_route_tables(G,old_L,a[0])
+
     count = 0
+    df.to_csv(f'./simulations/{count}_N{len(G.vs)-1}_C{changes}.csv')
     while count<changes:#Here the graph is updated and the routing tables are recalculeted
         iteraciones += 1
         G, old, new = update_graph(G) #Changes the weights on some of the edges of the graph and saves the old and the new weights.
@@ -58,7 +60,9 @@ def loop_update(G:Graph, n:Double, changes:int)->None:
         # a = [u, v]
         old_L, old_S = Dijkstra(G, a[0],vertices_afectados,old_L,old_S) #Uses DIjkstra but keeping the past calculations that where not affected by the update. Only recalculating on the affected vertices.
         count +=1 #Another recalculation was made
-        print_route_tables(G,old_L,a[0],count)
+        df = print_route_tables(G,old_L,a[0],count)
+        df.to_csv(f'./simulations/{count}_N{len(G.vs)-1}_C{changes}.csv')
+
         time.sleep(n)#Sleep time till next update/change in the graph
 
 #Input: g graph in igraph format, L a list of lists that represent the graph using the sparse matrix nodeA||nodeB||weight where A and B are adjacent nodes, u name of a node of the graph
@@ -70,10 +74,24 @@ def print_route_tables(g,L,u,count='inicial'):
     grafo = g
     print(f"--- Tabla de enrutamiento {count}-----")
     print("|Origen| Destino | Remitir paquete a | Peso| ")
+    d = {'Origen': [], 'Destino': [], 'Remitir paquete a': [], 'Peso': []}
     all_paths = list_graph_path(grafo,u,L) #paths from u to all nodes in a dictionary.
     for path in all_paths: #For each node in the graph different from u.
         iteraciones += 1
-        print(f"|{u}    |{path}       |         {all_paths[path][0][1]}        |  {all_paths[path][1]} |") #The last two elements of this print are: the first node in the path from  u to z and second is the total weight of that path.
+        try:
+            print(f"|{u}    |{path}       |         {all_paths[path][0][1]}        |  {all_paths[path][1]} |") #The last two elements of this print are: the first node in the path from  u to z and second is the total weight of that path.
+        except:
+            print(path)
+            print(all_paths[path])
+            raise
+
+        d['Origen'].append(u)
+        d['Destino'].append(path)
+        d['Remitir paquete a'].append(all_paths[path][0][1])
+        d['Peso'].append(all_paths[path][1])
+
+    df = pd.DataFrame(data = d)
+
     al = list(all_paths.values()) #Lists of lists with each list containing the path from u to z in a list and the total weight of the path.
     paths = [i[0] for i in al] #A list of lists with each list having a path from u to any node in the graph
     ##Creates png of the route in a reduced graph
@@ -91,26 +109,42 @@ def print_route_tables(g,L,u,count='inicial'):
     g.vs["label"] = g.vs["name"]
     g.es["label"] = g.es["weight"]
     plot(grafo, layout=layout,target= f"Este es el camino más corto en la actualización {count}.png")
+    return df
 
+def format_graph(t : list) -> Graph:
+    new_t = [(str(i[0]), str(i[1]), i[2]) for i in t]
+    return Graph.TupleList(new_t, weights = True)
+
+def simulations():
+    graph_sizes = [15,25,50,100]
+    changes_ids =[2,10]
+    times= []
+
+    for changes in changes_ids:
+        for nodes in graph_sizes:
+            start_time = time.time()
+            print(f"-----------------------\n--------- Nodos: {nodes} Cambios: {changes}------------\n---------------------------------")
+            loop_update(format_graph(radngraph.gen_graph(nodes)), 1, changes)
+            # times.append(time.time()- start_time())
+            
 
 def main():
+    start_time = time.time() #Starts the stopwatch to see how much time it took to execute and get the desired result
     with open("config.json", 'r') as f:
         config = json.loads(f.read())
 
-    print("Bienvenido a este algoritmo. A partir de una red y mediante el uso de grafos, se hallarán las tablas de enrutamiento dinámicamente. \n Atención: Se guardarán imágenes de los grafos y caminos calculados en la misma carpeta.")
+    print("Bienvenido a este algoritmo. Apartir de una red y mediante el uso de grafos, se hallarán las tablas de enrutamiento dinámicamente. \n Atención: Se guardarán imágenes de los grafos y caminos calculados en la misma carpeta.")
     if bool(config["generate_graph"]):
         n = config["nodes"]
         # changes = int(input("Ingrese el número de cambios a realizar"))#Falta pasarselo al loop_update 
         t  = radngraph.gen_graph(n)
-        t2 = [(str(i[0]), str(i[1]), i[2]) for i in t]
         radngraph.graph2csv(t)
     else:
         # a = input('Por favor ingrese el nombre del archivo de donde se generara el grafo(sin ".csv"): ')
         # b = a + '.csv'
         t = radngraph.import_graph(config["filename"] +".csv")
-        t2 = [(str(i[0]), str(i[1]), i[2]) for i in t]
 
-    g = Graph.TupleList(t2, weights=True)
+    g = format_graph(t)
     g.vs["label"] = g.vs["name"]
     g.es["label"] = g.es["weight"]
     layout = g.layout("kk")
@@ -120,4 +154,5 @@ def main():
     print("--- %s seconds ---" % (time.time() - start_time-4*2)) #Se restan 4*2 segundos porque ese fue el tiempo de espera para cambiar la información de las aristas.
 
 if __name__ == '__main__':
-    main()
+    # main()
+    simulations()
