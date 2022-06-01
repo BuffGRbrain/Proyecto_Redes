71d542ba2f0b094e5f0f43a1d54fb10f471667ab
BuffGRbrain <guillermo.ribero@urosario.edu.co>
2022-06-01 09:01:37 -0500

    cosa

--- Parte2-Paper/Scripts/DJ.py
+++ Parte2-Paper/Scripts/DJ.py
@@ -2,8 +2,8 @@ from igraph import *
 
 iteraciones = 0
 
-#This function request an L graph in dictionary format in which the keys are the node name, and the values are a list with the
-#weight and destiny node of the edge. This function finds a path between u and z.
+#Input: Graph L in dictionary format, initial node u, destiny node z.
+#Ouput: r a list of the nodes form the origin to the destiny node.
 
 def get_path(L, u, z, r=[]):
     global iteraciones
@@ -17,8 +17,9 @@ def get_path(L, u, z, r=[]):
 
 
 
-#This function asks for a graph G in iGraph normal format and 2 nodes with and edge between them (adjacent). 
-#This finds the weight of the edge between the nodes.
+
+#Input: Graph G in iGraph format, node x and node v of the graph.
+#Output: The weight of the edge between these nodes.
 
 def w(G, x, v):
     try:
@@ -29,10 +30,11 @@ def w(G, x, v):
 
 
 #Input:G graph from igraph, u a node in L and L is a list of lists each one with the node and its connected edges.
-#Output: Path from U to each node in the graph using the function get_path.
+#Output: Path from U to each node in the graph using the function get_path in a dictionary where keys are the name of the nodes 
+#and the values are a list of 2 elements: 1.List of the path from u to z. 2 .Total weight of going from u to z using the given path.
 
 def list_graph_path(G, u, L):
-    all_paths={}
+    all_paths={} #Dictionary with keys as the name of the node and values the path from u top that node.
     l = list(G.vs['name']) #List in which we have all the nodes of the graph.
     l.remove(str(u)) #Delete the initial node from the nodes to check.
     for i in l:
@@ -41,11 +43,11 @@ def list_graph_path(G, u, L):
         path = get_path(L, str(u), str(i), []) #Find paths from u to all the nodes with get_path.
         path = path[::-1] #It inverse the path, this because geth_path returns the route list backwards.
         path.append(i) #Appends the destiny node to the list because get_path makes the list without him.
-        if L[i][0] == float('inf'):
+        if L[i][0] == 0: #Float(inf). If the weight of the path is 0 is because there isn't any.
             path = []
             
-        all_paths[i] = [path,L[i][0]]
-    return all_paths
+        all_paths[i] = [path,L[i][0]] #Assign a path and a weight to their respective nodes.
+    return all_paths 
 
 #Input: G graph from iGraph, u an origin node and some boolean parameters that idk.
 #Output: l a list of lists that represent the graph using the sparse matrix nodeA||nodeB||weight where A and B are adjacent nodes.
@@ -57,10 +59,11 @@ def Dijkstra(G, u, affected_nodes = True, old_S = True, old_L = True):
         L[str(u)] = [0, []] #Changes value of distance from u to u to 0.
         S = [str(u)] #Now we append u to the list of checked nodes.
     else:
-        for node in old_S:
-            if node in affected_nodes:
-                    S = old_S[0:int(old_S.index(node)+1)]  #IDFK No fucking idea whats happening here but this is for the dynamic case.
-                    L = {i: L[i] for i in S}
+        #When we have to recalculate due to changes we use this case in which affected nodes is a set({}) of the affected nodes
+        for node in old_S: #For each node in the past revision in the past dijkstra
+            if node in affected_nodes:#for each affected node we make the recalculation
+                    S = old_S[0:int(old_S.index(node)+1)]  #Creates the S list of checked nodes, usign slices and "cutting off" the affected nodes to be checked again
+                    L = {i: old_L[i] for i in S} #Saves the past weights that where not affected by the update in the graph, reducing calculations
     start = 1
     while 1:
         L_S = {i: L[i][0] for i in L if i not in S} #Append to L_S all the nodes in L that have not been checked.
@@ -70,20 +73,13 @@ def Dijkstra(G, u, affected_nodes = True, old_S = True, old_L = True):
             x = u #x will be the node whose edges will be checked.
             start = 0 #Changing start to 0 because we will no longer be in the first node.
         else:
-            x = min(L_S, key=L_S.get) #Minimum of the weights
-            S.append(x)
+            x = min(L_S, key=L_S.get) #Selects the minimum of the weights and select that node to move to him.
+            S.append(x) #Indicates that this node is checked now.
 
         for v in G.vs["name"]: #For all the nodes in G.
             iteraciones +=1
             if v not in S: #If v hasn't been checked.
-                if L[str(v)][0] > L[str(x)][0] + w(G, str(x), str(v)):# and L[str(v)][0]!= float('inf') and L[str(x)][0] != float('inf'): #Mirar el caso de nodos no conectados al origen.
-                    L[str(v)][1].append(str(x))
-                    #L[str(v)] = [L[str(x)][0] + w(G, str(x), str(v)), L[str(v)][1]]#Updates weight of the edge and adds the route
-                    L[str(v)][0] = L[str(x)][0] + w(G, str(x), str(v))
+                if L[str(v)][0] > L[str(x)][0] + w(G, str(x), str(v)): #If the weight that are in L is greater than the route for a node we replace that.
+                    L[str(v)][1].append(str(x)) #If they are connected, we append in the predecessor list of v x.
+                    L[str(v)][0] = L[str(x)][0] + w(G, str(x), str(v)) #Updates weight of the edge and adds the route
     return L,S
-
-def actualizar_Dijkstra():
-    pass
-
-def compareGraph():
-    pass
--- Parte2-Paper/Scripts/main.py
+++ Parte2-Paper/Scripts/main.py
@@ -1,3 +1,4 @@
+from tokenize import Double
 from matplotlib.pyplot import plot
 from igraph import *
 from functools import cache
@@ -25,24 +26,25 @@ def update_graph(G):
     new_graph_weights = G.es['weight'] #Saves the new weights of the graph
     return G, old_graph_weights, new_graph_weights
 
-#Input: G graph from igraph, n number of nodes
-#Output: None in python, 
-def loop_update(G, n):#Falta criterio de parada
+#Input: G graph from igraph, n time sleep till the next change, changes int number of random updates in the graph
+#Output: None in python. 
+def loop_update(G:Graph, n:Double, changes:int)->None:
     global iteraciones
     iteraciones +=1
-    a = random.sample(G.vs['name'], 1)
-    old_L, old_S = Dijkstra(G,a[0]) #Dijkstra inicial
+    a = random.sample(G.vs['name'], 1) #Picks a random node in the graph
+    old_L, old_S = Dijkstra(G,a[0]) #First dijkstra to know the state of the graph
     print_route_tables(G,old_L,a[0])
     count = 0
-    while count<3:
+    while count<changes:#Here the graph is updated and the routing tables are recalculeted
         iteraciones += 1
-        G, old, new = update_graph(G) #
-        #comparar
-        indices_diferencias = [i for i in range(len(old)) if old[i]!=new[i]] #Da el indice donde los pesos son diferentes
+        G, old, new = update_graph(G) #Changes the weights on some of the edges of the graph and saves the old and the new weights.
+        #From here we detect the changes on the graph and make the correspondent calculations
+        indices_diferencias = [i for i in range(len(old)) if old[i]!=new[i]] #Compare the old and the new weights to find the indices of the weights that changed and using this, we can find what edges changed
         #edges = [G.es[i] for i in indices_diferencias] #Para tener las aristas que se cambiaron
         #print(edges) #Esto debe imprimir objetos de igraph
+        #Now we use the indices to find the correspondent edges and with them we save both of the nodes connected to that edge to the a set to know whitch vertices route should be recalculated
         vertices_afectados = set({})
-        for i in indices_diferencias:#Para tener los vertices que se cambiaron
+        for i in indices_diferencias:#Finding the vertices that where affected
             iteraciones += 1
             vertices_afectados.add(G.es[i].source)
             vertices_afectados.add(G.es[i].target)
@@ -52,77 +54,50 @@ def loop_update(G, n):#Falta criterio de parada
         #u = input('Nombre del nodo 1: ')
         #v = input('Nombre del nodo 2: ')
         # a = [u, v]
-        old_L, old_S = Dijkstra(G, a[0],vertices_afectados,old_L,old_S) #Este terecer input da los vertices afectados
-        count +=1
+        old_L, old_S = Dijkstra(G, a[0],vertices_afectados,old_L,old_S) #Uses DIjkstra but keeping the past calculations that where not affected by the update. Only recalculating on the affected vertices.
+        count +=1 #Another recalculation was made
         print_route_tables(G,old_L,a[0],count)
-        time.sleep(n)
+        time.sleep(n)#Sleep time till next update/change in the graph
 
-#Input:
-#Output:
+#Input: g graph in igraph format, L a list of lists that represent the graph using the sparse matrix nodeA||nodeB||weight where A and B are adjacent nodes, u name of a node of the graph
+#and count a str to know in which table the data goes? check
+#Output: None in python, but it shows the routing tables in the console.
+
 def print_route_tables(g,L,u,count='inicial'): 
     global iteraciones
     grafo = g
     print(f"--- Tabla de enrutamiento {count}-----")
     print("|Origen| Destino | Remitir paquete a | Peso| ")
-    all_paths = list_graph_path(grafo,u,L)
-    for path in all_paths:
+    all_paths = list_graph_path(grafo,u,L) #paths from u to all nodes in a dictionary.
+    for path in all_paths: #For each node in the graph different from u.
         iteraciones += 1
-        print(f"|{u}    |{path}       |         {all_paths[path][0][1]}        |  {all_paths[path][1]} |")
-    al = list(all_paths.values())
-    paths = [i[0] for i in al]
+        print(f"|{u}    |{path}       |         {all_paths[path][0][1]}        |  {all_paths[path][1]} |") #The last two elements of this print are: the first node in the path from  u to z and second is the total weight of that path.
+    al = list(all_paths.values()) #Lists of lists with each list containing the path from u to z in a list and the total weight of the path.
+    paths = [i[0] for i in al] #A list of lists with each list having a path from u to any node in the graph
+    ##Creates png of the route in a reduced graph
     AU = []
     for i in paths:
-        for j in range(len(i)-1):
+        for j in range(len(i)-1): #Taking the nodes of each route
             iteraciones += 1
-            AU.append(grafo.get_eid(str(i[j]), str(i[j+1])))
-    AU = list(set(AU))
-    AU = [grafo.es[i] for i in AU]
+            AU.append(grafo.get_eid(str(i[j]), str(i[j+1]))) #Toma cada ruta y va agregando a AU los ids de las aristas que se usan en cada ruta.
+    AU = list(set(AU)) #Eliminan ids repetidos.
+    AU = [grafo.es[i] for i in AU] #Using the id we get the object edge from igraph
     NU = [i for i in grafo.es if i not in AU]
     grafo.delete_edges(NU)
+    ##Creates png of the route in a reduced graph
     layout = grafo.layout("kk")
     g.vs["label"] = g.vs["name"]
     g.es["label"] = g.es["weight"]
     plot(grafo, layout=layout,target= f"Este es el camino más corto en la actualización {count}.png")
 
-def show_weihtges(G):
-    for i in G.es:
-        source = G.vs(i.source)['name']
-        target = G.vs(i.target)['name']
-        weight = i['weight']
-        print(f'{source} --> {target}: {weight}')
-
-'''
-def full_dijkstra(G, u):
-    l = G.vs['name']
-    l.remove(u)
-    for i in l:
-        Dijkstra(G, u, i)
-'''
 
 def main():
-
-    # t = [('a', 'b', 0), ('a', 'c', 0), ('a', 'd', 0),
-    #     ('b', 'c', 0), ('b', 'd', 0)]
-    #t = [[32, 29, 2], [29, 22, 15], [32, 43, 3], [43, 45, 7], [29, 33, 7], [45, 24, 5], [33, 17, 10], [43, 38, 11], [24, 21, 9], [17, 34, 8], [22, 47, 13], [29, 15, 13], [33, 25, 10], [17, 30, 4], [24, 16, 2], [22, 28, 5], [22, 31, 15], [43, 36, 8], [34, 37, 2], [34, 23, 9], [21, 20, 13], [29, 27, 14], [33, 46, 12], [15, 18, 1], [
-        #29, 35, 3], [25, 19, 9], [43, 44, 1], [47, 42, 1], [25, 41, 14], [20, 40, 15], [38, 26, 9], [46, 39, 8], [45, 18, 5], [16, 18, 1], [40, 45, 8], [41, 24, 15], [27, 46, 2], [27, 25, 7], [21, 26, 9], [40, 26, 8], [19, 46, 13], [21, 42, 3], [35, 44, 1], [30, 15, 8], [29, 34, 8], [23, 18, 15], [37, 33, 8], [31, 28, 2]]
-     #Usa dijkstra y cada cierto tiempo modifica el grafo
-    # gl = threading.Thread(target=loop_update_update, args=(g, 2))
-    # dl = threading.Thread(target=loop_update_Dijkstra, args=(g))
-
-    # gl.start()
-    # dl.start()
-    # show_weihtges(g)
-    # update_graph(g)
-    # print('--------')
-    # show_weihtges(g)
     print("Bienvenido a este algoritmo. A partir de una red y mediante el uso de grafos, se hallarán las tablas de enrutamiento dinámicamente. \n Atención: Se guardarán imágenes de los grafos y caminos calculados en la misma carpeta.")
     if int(input('0 - Cargar Grafo \n1 - Generar Grafo \n Seleccion: ')):
         n = int(input("Por favor ingrese el numero de nodos del grafo de 15 a 50: "))
-        #print(n)
+        changes = int(input("Ingrese el número de cambios a realizar"))#Falta pasarselo al loop_update 
         t  = radngraph.gen_graph(n)
         t2 = [(str(i[0]), str(i[1]), i[2]) for i in t]
-        #t = Conex_graph_generator(n)
-        #print(t)
         radngraph.graph2csv(t)
     else:
         a = input('Por favor ingrese el nombre del archivo de donde se generara el grafo(sin ".csv"): ')
