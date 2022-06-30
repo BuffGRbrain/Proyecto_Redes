1f1a3acf9f3658d64eb60f331087dd2e4e574faf
dleyvacastro <dleyvacastro@gmail.com>
2022-06-29 19:06:33 -0500

    minor changes, refactor, some coments, missing better doc

--- Parte2-Paper/Scripts/DJ.py
+++ Parte2-Paper/Scripts/DJ.py
@@ -1,51 +1,47 @@
-from typing import Dict, List, Any
 import pandas as pd
-
 from igraph import *
-errorss = 0
-iteraciones = 0
 
-#Input: Graph L in dictionary format, initial node u, destiny node z.
-#Ouput: r a list of the nodes form the origin to the destiny node.
+errorss = 0
+
+# Input: Graph L in dictionary format, initial node u, destiny node z.
+# Ouput: r a list of the nodes form the origin to the destiny node.
 
 def get_path(L, u, z, r=[]):
-
-    lz = L[z][1] #It selects the first edge of the destiny node.
-    r.append(lz[-1]) #Here it appends the previous node of the destiny node.
+    lz = L[z][1]  # It selects the first edge of the destiny node.
+    r.append(lz[-1])  # Here it appends the previous node of the destiny node.
     if u in r:
-        return r #It ends if the origin node enters in the list of the route edges. Returns the path from z to u.
-    return get_path(L, u, lz[-1], r) #If the origin node isn't in r, the procedure repeats with the obtained node.
+        return r  # It ends if the origin node enters in the list of the route edges. Returns the path from z to u.
+    return get_path(L, u, lz[-1], r)  # If the origin node isn't in r, the procedure repeats with the obtained node.
 
 
-#Input: Graph G in iGraph format, node x and node v of the graph.
-#Output: The weight of the edge between these nodes.
+# Input: Graph G in iGraph format, node x and node v of the graph.
+# Output: The weight of the edge between these nodes.
 
 def w(G, x, v):
     try:
-        return G.es[G.get_eid(x, v)]["weight"] #If an edge exists between the nodes this finds the weight of it.
+        return G.es[G.get_eid(x, v)]["weight"]  # If an edge exists between the nodes this finds the weight of it.
     except:
-        return float('inf') #If there is no edge exists between the nodes this returns infinite.
+        return float('inf')  # If there is no edge exists between the nodes this returns infinite.
 
 
-
-#Input:G graph from igraph, u a node in L and L is a dictionary in which the keys are the nodes and the values are a weight and destiny node of the edge.
-#Output: Path from U to each node in the graph using the function get_path in a dictionary where keys are the name of the nodes
-#and the values are a list of 2 elements: 1.List of the path from u to z. 2 .Total weight of going from u to z using the given path.
+# Input:G graph from igraph, u a node in L and L is a dictionary in which the keys are the nodes and the values are a weight and destiny node of the edge.
+# Output: Path from U to each node in the graph using the function get_path in a dictionary where keys are the name of the nodes
+# and the values are a list of 2 elements: 1.List of the path from u to z. 2 .Total weight of going from u to z using the given path.
 
 def list_graph_path(G, u, L):
     print("DEPRECATED")
-    all_paths={} #Dictionary with keys as the name of the node and values the path from u top that node.
-    l = list(G.vs['name']) #List in which we have all the nodes of the graph.
-    l.remove(str(u)) #Delete the initial node from the nodes to check.
+    all_paths = {}  # Dictionary with keys as the name of the node and values the path from u top that node.
+    l = list(G.vs['name'])  # List in which we have all the nodes of the graph.
+    l.remove(str(u))  # Delete the initial node from the nodes to check.
 
     for i in l:
-        path = get_path(L, str(u), str(i), []) #Find paths from u to all the nodes with get_path.
-        path = path[::-1] #It inverse the path, this because geth_path returns the route list backwards.
-        path.append(i) #Appends the destiny node to the list because get_path makes the list without him.
+        path = get_path(L, str(u), str(i), [])  # Find paths from u to all the nodes with get_path.
+        path = path[::-1]  # It inverse the path, this because geth_path returns the route list backwards.
+        path.append(i)  # Appends the destiny node to the list because get_path makes the list without him.
         # if L[i][0] == 0: #Float(inf). If the weight of the path is 0 is because there isn't any.
         #    path = []
 
-        all_paths[i] = [path,L[i][0]] #Assign a path and a weight to their respective nodes.
+        all_paths[i] = [path, L[i][0]]  # Assign a path and a weight to their respective nodes.
     return all_paths
 
 
@@ -54,7 +50,7 @@ def to_disperse_matrix(t: list, L: dict) -> list:
     # Output : list of lists of weights to disperse matrix.
 
     bt = {tuple(sorted((i[0], i[1]))): i[2] for i in t}
-    nt =[]
+    nt = []
     for i in L:
         try:
             pred = L[i][1][0]
@@ -67,9 +63,10 @@ def to_disperse_matrix(t: list, L: dict) -> list:
         nt.append(tmp)
     return nt
 
-def get_routing_table(t: list, L: list, u : str):
+
+def get_routing_table(t: list, L: dict, u: str):
     bt = {tuple(sorted((i[0], i[1]))): i[2] for i in t}
-    rt = {"Source":[], "Destination":[], "Predecessor": [], "Weight":[]}
+    rt = {"Source": [], "Destination": [], "Predecessor": [], "Weight": []}
     for i in L:
         try:
             if not (int(i) == int(u)):
@@ -79,30 +76,29 @@ def get_routing_table(t: list, L: list, u : str):
                 rt["Predecessor"].append(int(L[i][1][0]))
         except:
             continue
-    return pd.DataFrame(data = rt)
+    return pd.DataFrame(data=rt)
 
 
+def Dijkstra(G: Graph, u: str, iterations: int = 0, affected_nodes: list = [], old_S: list = [], old_L: dict = {}) -> (
+dict, list, int):
+    # Input: G graph from iGraph.
+    #        u : node in the graph.
+    #        iterations : number of iterations.
+    # Output: L a list of lists that represent the graph using the sparse matrix nodeA||nodeB||weight where A and B are adjacent nodes.
+    #         S a list of nodes that are in the shortest path from u to all the nodes in the graph.
+    #         iterations the number of iterations that the algorithm has done.
 
-
-#Input: G graph from iGraph, u an origin node and some boolean parameters that idk.
-#Output: l a list of lists that represent the graph using the sparse matrix nodeA||nodeB||weight where A and B are adjacent nodes.
-
-def Dijkstra(G, u, affected_nodes = [], old_S = [], old_L = {}):
-    #print(f"------------------------------------------dij")
-    print(affected_nodes)
-    print(old_S)
-    global iteraciones
     global errorss
     L = {i: [float('inf'), []] for i in G.vs["name"]}  # Initializes all distances from u to any node in infinite.
     L[str(u)] = [0, []]  # Changes value of distance from u to u to 0.
     start2 = False
     if not old_S:
-        S = [str(u)] #Now we append u to the list of checked nodes.
+        S = [str(u)]  # Now we append u to the list of checked nodes.
         start = True
     else:
-        #affected_nodes = [str(i) for i in affected_nodes]
+        # affected_nodes = [str(i) for i in affected_nodes]
         start = False
-        #When we have to recalculate due to changes we use this case in which affected nodes is a set({}) of the affected nodes
+        # When we have to recalculate due to changes we use this case in which affected nodes is a set({}) of the affected nodes
 
         S = []
         for i in old_L:
@@ -115,35 +111,37 @@ def Dijkstra(G, u, affected_nodes = [], old_S = [], old_L = {}):
 
     if 'S' in locals():
         while 1:
-            L_S = {i: L[i][0] for i in L if i not in S} #Append to L_S all the nodes in L that have not been checked.
+            L_S = {i: L[i][0] for i in L if i not in S}  # Append to L_S all the nodes in L that have not been checked.
             # print(f"LS: {L_S} PUTA LISTA")
-            if not L_S: #If all nodes have been checked, it breaks, if not it continues.
-                break 
-            if start: #If we are in the first node, we asign u to x. 
-                x = u #x will be the node whose edges will be checked.
-                start = False #Changing start to 0 because we will no longer be in the first node.
+            if not L_S:  # If all nodes have been checked, it breaks, if not it continues.
+                break
+            if start:  # If we are in the first node, we asign u to x.
+                x = u  # x will be the node whose edges will be checked.
+                start = False  # Changing start to 0 because we will no longer be in the first node.
             elif start2:
                 x = S[-1]
                 start2 = False
             else:
-                x = min(L_S, key=L_S.get) #Selects the minimum of the weights and select that node to move to him
+                x = min(L_S, key=L_S.get)  # Selects the minimum of the weights and select that node to move to him
                 if L[x][0] == float('inf'):
                     raise Exception("No path found")
-                S.append(x) #Indicates that this node is checked now.
+                S.append(x)  # Indicates that this node is checked now.
 
-            for v in G.vs["name"]: #For all the nodes in G.
-                iteraciones +=1
-                if v not in S: #If v hasn't been checked.
-                    if L[str(v)][0] > L[str(x)][0] + w(G, str(x), str(v)): #If the weight that are in L is greater than the route for a node we replace that.
+            for v in G.vs["name"]:  # For all the nodes in G.
+                iterations += 1
+                if v not in S:  # If v hasn't been checked.
+                    if L[str(v)][0] > L[str(x)][0] + w(G, str(x),
+                                                       str(v)):  # If the weight that are in L is greater than the route for a node we replace that.
                         try:
                             L[str(v)][1].pop()
                         except:
                             pass
-                        L[str(v)][1].append(str(x)) #If they are connected, we append in the predecessor list of v x.
-                        L[str(v)][0] = L[str(x)][0] + w(G, str(x), str(v)) #Updates weight of the edge and adds the route
-        return L,S
+                        L[str(v)][1].append(str(x))  # If they are connected, we append in the predecessor list of v x.
+                        L[str(v)][0] = L[str(x)][0] + w(G, str(x),
+                                                        str(v))  # Updates weight of the edge and adds the route
+        return L, S, iterations
 
     else:
         errorss += 1
         print("Grafo picho")
-        return old_L,old_S
+        return old_L, old_S
